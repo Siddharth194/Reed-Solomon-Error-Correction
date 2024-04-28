ed93db9b058e75404caef255d451bc87b64b52be
Siddharth194 <83445812+Siddharth194@users.noreply.github.com>
2024-04-28 16:19:11 +0530

    Transmit completed

--- ReedSolomonSend.py
+++ ReedSolomonSend.py
@@ -2,6 +2,7 @@ import pickle
 import gmpy2
 import basic_ops.modulo as modulo
 from basic_ops.power import *
+from ReedSolomonTransmit import *
 
 with open("global_vars.bin", "rb+") as file:
     global_variables = pickle.load(file)
@@ -12,4 +13,6 @@ ai_array = []
 
 for i in global_variables.pi_array:
     ai_array.append(modulo.modulo(a,i))
-    
+
+print(ai_array)
+Transmit(ai_array)
--- ReedSolomonTransmit.py
+++ ReedSolomonTransmit.py
@@ -2,12 +2,24 @@ import random
 import basic_ops.modulo as modulo
 from basic_ops.power import *
 import pickle
+import copy
 
-with open("global_vars.bin", "rb+") as file:
-    global_variables = pickle.load(file)
+def Transmit(ai_array):
+    with open("global_vars.bin", "rb+") as file:
+        global_variables = pickle.load(file)
 
-k_corrupted = global_variables.nu * global_variables.k                      # number of indexes that get corrupted during transmission
-corr_indexes = random.sample(range(0, global_variables.k), k_corrupted)     # picking the corrupted indexes at random
+    k_corrupted = global_variables.nu * global_variables.k                           # number of indexes that get corrupted during transmission
+    corr_indexes = random.sample(range(0, global_variables.k), int(k_corrupted))     # picking the corrupted indexes at random
+    # print(corr_indexes)
+    bi_array = copy.deepcopy(ai_array)
 
-bi_array = 
-for i in corr_indexes:
+    for i in corr_indexes:
+        while (bi_array[i] ==  ai_array[i]):
+            bi_array[i] = random.randint(1, global_variables.pi_array[i])
+    
+    print(bi_array)
+
+    with open("channel.bin","wb+") as file:
+        pickle.dump(bi_array,file)
+
+
