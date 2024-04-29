05a01fb6e94bec88046413350678085ee2a3d83a
vrajnandak <112724045+vrajnandak@users.noreply.github.com>
2024-04-29 12:24:55 +0530

    Update GlobalSetup.py

--- GlobalSetup.py
+++ GlobalSetup.py
@@ -4,22 +4,24 @@ import gmpy2
 from global_variable_setup import *
 import pickle
 
-global_vars = Global_Variables()
 
-while True:                         # generation of the array of k primes, p1, p2, ..., pk
-    k = random.randint(1,3000)
-    if (MillerRabin.MillerRabin(k) and k not in global_vars.pi_array):
-        print(k)
-        global_vars.pi_array.append(k)
-        global_vars.size += 1
+def GlobalSetup(nu,M):
+    global_vars = Global_Variables(nu,M)
 
-    else:
-        continue
+    print('Printing primes:')
+    while True:                         # generation of the array of k primes, p1, p2, ..., pk
+        k = random.randint(1,3000)
+        if (MillerRabin.MillerRabin(k) and k not in global_vars.pi_array):
+            print(k)
+            global_vars.pi_array.append(k)
+            global_vars.size += 1
 
-    if global_vars.size == global_vars.k:
-        break
+        else:
+            continue
 
+        if global_vars.size == global_vars.k:
+            break
 
-# Save global_vars object to a binary file
-with open("global_vars.bin", "wb") as file:
-    pickle.dump(global_vars, file)
+    # Save global_vars object to a binary file
+    with open("global_vars.bin", "wb") as file:
+        pickle.dump(global_vars, file)
