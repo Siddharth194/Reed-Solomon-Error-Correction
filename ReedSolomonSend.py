844c14eb4b6e81400771091cc97e34f8eb9d5076
vrajnandak <112724045+vrajnandak@users.noreply.github.com>
2024-04-29 10:11:48 +0530

    Update ReedSolomonSend.py

--- ReedSolomonSend.py
+++ ReedSolomonSend.py
@@ -4,15 +4,19 @@ import basic_ops.modulo as modulo
 from basic_ops.power import *
 from ReedSolomonTransmit import *
 
+#Reading the global variables.
 with open("global_vars.bin", "rb+") as file:
     global_variables = pickle.load(file)
 
-a = 235457
+#The message to send.
+a = 2354537
 
+#Generating the sequence ai defined as ai is congruent to a(mod pi).
 ai_array = []
-
 for i in global_variables.pi_array:
     ai_array.append(modulo.modulo(a,i))
 
-print(ai_array)
+print('Original sequence: ',ai_array)
+
+#Transmitting the Sequence.
 Transmit(ai_array)
