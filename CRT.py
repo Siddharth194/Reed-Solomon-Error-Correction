0a8d2300a5d3003cbdad98b8019377514fa5af29
vrajnandak <112724045+vrajnandak@users.noreply.github.com>
2024-04-29 10:09:09 +0530

    Update CRT.py

--- CRT.py
+++ CRT.py
@@ -4,20 +4,21 @@ from basic_ops.division import *
 from EGCD_a_b import *
 
 def resolve_crt(list_a,list_n):
-    n = 1
+    #Calculating the product of all the primes.
+    n=1
     for i in list_n:
-        n = n * i
+        n=n*i
 
-    solution = 0
-    X = 0
-    for i in range(0, len(list_a)):
-        #Binary_EGCD(Ni,ni)
-        Ni = division(n, list_n[i])
-        #print('herre',Ni, 'and',list_n[i])
-        d, s, t = BinaryEGCD(Ni, list_n[i])
-        X = X + (list_a[i] * s * Ni)
+    solution=0
+    X=0
 
-    #Calculate Xmodn
-    solution = modulo(X, n)
+    #Calculating X=sigma(Xi*ai*Ni) for all 'i' from 0 to k-1 as defined in class.
+    for i in range(0,len(list_a)):
+        Ni=division(n,list_n[i])
+        d,s,t=BinaryEGCD(Ni,list_n[i])
+        X=X+(list_a[i]*s*Ni)
 
-    return solution, n
+    #Calculate X(modn)
+    solution=modulo.modulo(X,n)
+
+    return solution,n
